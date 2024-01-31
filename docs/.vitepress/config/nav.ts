800481ec37ed4f99ed0298838f53850e0c6802d7
9sky <xmsn90@qq.com>
2024-01-31 19:13:50 +0800

    update blog

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -39,10 +39,7 @@ export const nav: DefaultTheme.Config["nav"] = [
   },
   {
     text: "关于",
-    items: [
-      { text: "关于知识库", link: "/about/index", activeMatch: "/about/index" },
-      { text: "关于我", link: "/about/me", activeMatch: "/about/me" },
-    ],
-    activeMatch: "/about/",
+    items: [{ text: "关于我", link: "/about/me", activeMatch: "/about/me" }],
+    activeMatch: "/about/me",
   },
 ];
--- docs/about/index.md
+++
@@ -1,6 +0,0 @@
----
-title: 关于知识库
-aside: false
-editLink: false
-lastUpdated: false
----
