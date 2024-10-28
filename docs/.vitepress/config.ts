8450cb6e2b29c15c9a1ae10adab79db377e1457d
wty9sky <xmsn90@qq.com>
2024-10-28 14:48:55 +0800

    update config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,7 +10,7 @@ export default withMermaid(
     lang: metaData.lang,
     title: metaData.title,
     description: metaData.description,
-
+    base:'/blog_2023/',
     cleanUrls: true,
     lastUpdated: true, // 显示最后更新时间
 
